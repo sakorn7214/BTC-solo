94565f425484a7b23cec3067021fb3c540734e22
sakorn <101166970+sakorn7214@users.noreply.github.com>
2024-06-28 14:54:29 +0700

    Update config.py

--- src-miner/config.py
+++ src-miner/config.py
@@ -13,4 +13,4 @@ def banneredit():
     print("\033[1;34;34m")
     os.system("figlet -f  ANSI_Shadow EDIT")
     print("\033[00m\n")
-    print(f"\n\033[96mDeveloper by PICHET SAENGTEWAN\033[0m\n")
+    print(f"\n\033[96mDeveloper by สาคร ชูเชิด\033[0m\n")
